283b85e240bd5d58cc4cd3676b9ad8f933a5940b
Ronald Ekambi <ronald.ekambi@cenx.com>
2016-08-02 19:27:42 -0400

    moved everything to a js file

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	var scroll_begin = 0;
+	var projects = $('#projects');
+	var offset = projects.offset();
+	if(projects.length){
+		$(document).scroll(function(){
+			scroll_begin = $(this).scrollTop();
+			if(scroll_begin > offset.top){
+				$('.navbar').css('background-color', 'black');
+			}else{
+				$('.navbar').css('background-color', 'black');
+			}
+		}); 
+	}
+});
